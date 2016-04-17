46c8fb31b0b7aaab47ed26f7450f5950204c6d8e
gyaboyan <gyaboyan@gmail.com>
2016-04-17 13:30:07 +0900

    rename and update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# RaspberryPI bare metal playground." 
+# RaspberryPI bare metal playground." 
+
+# RPI2_TRIANGLE_DEMO
+Raspberry V3D Triangle demo, and DMA.
--- src/renderchunk.c
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-#include "hwio.h"
-volatile uint8_t renderchunk_buffer0[TILE_MEM_ALLOC_SIZE] __attribute__((aligned(256))) ;
-
-void init_render_chunk_buffer() {
-	memset_t(renderchunk_buffer0, 0x5A, sizeof(renderchunk_buffer0));
-}
